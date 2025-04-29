fc0c4d090544533b69690ffb48f38b7dd47060cb
QualicsScription <mznayir@gmail.com>
2025-04-29 12:09:24 +0300

    Düzenleme: ModernFileComparator import yolunu güncelle

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,7 @@
 # Main/main.py
 import sys
 from PyQt5.QtWidgets import QApplication
-from src.ui.ui import ModernFileComparator
+from src.ui.ui import ModernFileComparator  # Updated import path
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
